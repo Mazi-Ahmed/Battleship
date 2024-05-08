09412a6816eafb7cc9c4d9bf0a5e57d1b295587c
Mazi Ahmed <maziahmed@mazis-mbp.mynetworksettings.com>
2024-05-07 22:52:22 -0400

    Day 2 - Fixing Grid, Added HTML: Ship Pieces, Play Button, Display

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
+const playButton = document.querySelector('#play-button')
+const gridContainer = document.querySelector('#grid-container')
+
+// Play Button
+function play() {}
+
 // Create a Player Grid and Enemy Grid
-
 const gridSize = 10;
-let playerGrid = [];
-let enemyGrid = [];
 
-function createGrid(containerId,grid) {
-    const container = document.getElementById(containerId);
-    container.innerHTML = '';
-    for(let i = 0;i < gridSize; i++) {
-        const row = document.createElement('div');
-        row.classList.add('row');
-        for(let j = 0; j < gridSize; j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.dataset.row = i;
-        cell.dataset.col = j;
-        row.appendChild(cell);
-        grid.push({row: i, col: j, cell: cell});
-        }
-        container.appendChild(row)
-    }
+function createGrid() {
+    
 }
-createGrid('playerGrid', playerGrid)
+
+
+// const gridSize = 10;
+// let playerGrid = [];
+// let enemyGrid = [];
+
+// function createGrid(containerId,grid) {
+//     const container = document.getElementById(containerId);
+//     container.innerHTML = '';
+//     for(let i = 0;i < gridSize; i++) {
+//         const row = document.createElement('div');
+//         row.classList.add('row');
+//         for(let j = 0; j < gridSize; j++) {
+//         const cell = document.createElement('div');
+//         cell.classList.add('cell');
+//         cell.dataset.row = i;
+//         cell.dataset.col = j;
+//         row.appendChild(cell);
+//         grid.push({row: i, col: j, cell: cell});
+//         }
+//         container.appendChild(row)
+//     }
+// }
+// createGrid('playerGrid', playerGrid)
 
 
 
 
-class Ship {
-    constructor(name) {
-        this.name = name;
-        this.parts = parts;
+// class Ship {
+//     constructor(name) {
+//         this.name = name;
+//         this.parts = parts;
 
-    }
-}
+//     }
+// }
